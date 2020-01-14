216b66e9a06b43a4c7e7f006cbb7a66a8697ab5d
Aser <asermalik@live.com>
2020-01-14 17:13:10 -0500

    added antd and loaders, now using react rewired
    Loaders not working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import React, { Component } from "react";
-import ReactDOM from "react-dom";
+//import ReactDOM from "react-dom";
+import { Button } from "antd";
+import "./styles.css";
 
 export var section;
 export var card;
 
+var test = [7, "Hello World"];
+/*context data
+//////////////
+//////////////
+//////////////
+//////////////
+//////////////
+//////////////
+//////////////
+////////////*/
+
 export default function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <Button type="primary">Hello</Button>
+      {section("Hello World!")}
+      {card("1", test)}
+      {card("3", "4321")}
+      {section("Hello World!")}
+      {card("1", test)}
+      {card("3", "4321")}
+      {section("Hello World!")}
+      {card("1", test)}
+      {card("3", "4321")}
+    </div>
+  );
 }
 
 section = title => {
@@ -16,16 +42,16 @@ section = title => {
   );
 };
 
-card = (title, type) => {
+card = (title, cardDetails) => {
+  const listCardDetails = Array.from(cardDetails).map(cardDetails => (
+    <li>{cardDetails}</li>
+  ));
   return (
     <div className="card">
       <div className="card-context-image" />
       <div className="card-context-info">
         <p className="card-title">{title}</p>
-        <ul className="card-subtitle">
-          <li>{type[0]}</li>
-          <li>{type[1]}</li>
-        </ul>
+        <ul className="card-subtitle">{listCardDetails}</ul>
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import App from "./App.js";
-import { section, card } from "./App.js";
-
-var test = [7, "Hello World"];
 
 const rootElement = document.getElementById("root");
 
-ReactDOM.render(
-  <App>
-    {card("1", test)}
-    {card("3", "4")}
-  </App>,
-  rootElement
-);
+ReactDOM.render(<App />, rootElement);
